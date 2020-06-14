c3ca4975582077ac40f282d217a3a6eeef78e248
ujasthakkar <62230701+ujasthakkar@users.noreply.github.com>
2020-06-14 17:59:46 +0530

    Add files via upload

---
+++ trade.py
@@ -0,0 +1,18 @@
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
+
+
+start=dt.datetime(2000,1,1)
+end=dt.datetime(2016,12,31)
+
+df=web.DataReader('TSLA','yahoo',start,end)
+df.to_csv('TSLA.csv')
+
+df=pd.read_csv('TSLA.csv',parse_dates = True,index_col=0)
+print(df.head())
+
+df.plot()
+plt.show()
